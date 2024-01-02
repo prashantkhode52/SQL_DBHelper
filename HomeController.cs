0316273945c1cfae5f6781224e8756c55e5fd1ed
prashantkhode52 <110621007+prashantkhode52@users.noreply.github.com>
2024-01-02 18:14:06 +0530

    Add files via upload
    Raw Classes

---
+++ HomeController.cs
@@ -0,0 +1,58 @@
+﻿using MasterSoft.OBEDataAcessLayer;
+using SampleProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        SqlHelper objSQLHelper = new SqlHelper();
+        // GET: Home
+        public ActionResult Index()
+        {
+            DataSet ds = new DataSet();
+            SqlParameter[] objParams = null;
+            List<HomeModel> SessionList = new List<HomeModel>();
+            try
+            {
+                objParams = new SqlParameter[2];
+                objParams[0] = new SqlParameter("@Id", 0);
+                objParams[1] = new SqlParameter("@OrganizationId", Convert.ToInt32(Session["OrganizationId"]));
+                ds = objSQLHelper.ExecuteDataSetSP("sptblMaster_Get", objParams);
+
+                foreach (DataRow dr in ds.Tables[0].Rows)
+                {
+                    SessionList.Add(new HomeModel()
+                    {
+                       Id = int.Parse(dr["Id"].ToString()),
+                       Name = dr["Name"].ToString(),
+                       
+                    });
+                }
+
+            }
+            catch (Exception ex)
+            {
+                string Msg, Desc;
+                Msg = ex.Message;
+                Desc = ex.StackTrace;
+                if (Convert.ToInt32(Session["IsError"]) != 0)
+                {
+                    return RedirectToAction("CustomError", "Error", new { message = Msg, Desc = Desc });
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Error", new { message = Msg, Desc = Desc });
+                }
+            }
+            //return Json(new { data = SessionList }, JsonRequestBehavior.AllowGet);
+            return View();
+        }
+    }
+}
---
+++ SqlHelper.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace MasterSoft
+{
+    namespace OBEDataAcessLayer
+    {
+
+        public class SqlHelper
+        {
+            private string _constr = string.Empty;      //connection string
+
+            public SqlHelper(string connectionstring)
+            {
+                _constr = connectionstring;
+            }
+            public SqlHelper()
+            {
+
+            }
+            public string Getcon()
+            {
+                string ConnectionString = string.Empty;
+                return ConnectionString = ConfigurationManager.ConnectionStrings["DB_Con"].ConnectionString;
+
+            }
+        
+       
+            public object ExecuteNonQuerySP(String query, SqlParameter[] parameters, bool flag)
+            {
+
+                SqlConnection cnn = new SqlConnection(Getcon());
+                SqlCommand cmd = new SqlCommand(query, cnn);
+                cmd.CommandTimeout = 2000;
+                if (query.StartsWith("INSERT") | query.StartsWith("insert") | query.StartsWith("UPDATE") | query.StartsWith("update") | query.StartsWith("DELETE") | query.StartsWith("delete"))
+                    cmd.CommandType = CommandType.Text;
+                else
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                int i;
+                for (i = 0; i < parameters.Length; i++)
+                    cmd.Parameters.Add(parameters[i]);
+
+                object retval = null;
+                try
+                {
+                    cnn.Open();
+                    retval = cmd.ExecuteNonQuery();
+
+                    if (flag == true)
+                        retval = cmd.Parameters[parameters.Length - 1].Value;
+                }
+                catch (Exception ex)
+                {
+                    retval = null;
+
+                }
+                finally
+                {
+                    if (cnn.State == ConnectionState.Open) cnn.Close();
+                }
+                return retval;
+            }
+
+            public DataSet ExecuteDataSetSP(String query, SqlParameter[] parameters)
+            {
+                DataSet ds = null;
+                try
+                {
+                    SqlConnection cnn = new SqlConnection(Getcon());
+                    SqlCommand cmd = new SqlCommand(query, cnn);
+                    if (query.StartsWith("SELECT") | query.StartsWith("select") | query.StartsWith("INSERT") | query.StartsWith("insert") | query.StartsWith("UPDATE") | query.StartsWith("update") | query.StartsWith("DELETE") | query.StartsWith("delete"))
+                        cmd.CommandType = CommandType.Text;
+                    else
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                    int i;
+                    for (i = 0; i < parameters.Length; i++)
+                        cmd.Parameters.Add(parameters[i]);
+
+                    SqlDataAdapter da = new SqlDataAdapter();
+                    da.SelectCommand = cmd;
+                    cmd.CommandTimeout = 180;
+                    ds = new DataSet();
+                    da.Fill(ds);
+
+                }
+                catch (Exception ex)
+                {
+                    ds = null;
+
+                }
+                return ds;
+            }
+
+            public SqlDataReader ExecuteDataReaderSP(String query, SqlParameter[] parameters)
+            {
+
+                SqlDataReader dr = null;
+                using (SqlConnection cnn = new SqlConnection(Getcon()))
+                {
+                    try
+                    {
+                        using (SqlCommand cmd = new SqlCommand(query, cnn))
+                        {
+                            if (query.StartsWith("SELECT") | query.StartsWith("select"))
+                                cmd.CommandType = CommandType.Text;
+                            else
+                                cmd.CommandType = CommandType.StoredProcedure;
+
+                            int i;
+                            for (i = 0; i < parameters.Length; i++)
+                                cmd.Parameters.Add(parameters[i]);
+
+                            cnn.Open();
+                            dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                            return dr;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        dr = null;
+
+                        throw;
+                    }
+
+                }
+
+            }
+
+            public Object ExecuteScalarSP(String query, SqlParameter[] parameters)
+            {
+                Object retval;
+                using (SqlConnection cnn = new SqlConnection(Getcon()))
+                {
+                    using (SqlCommand cmd = new SqlCommand(query, cnn))
+                    {
+                        if (query.StartsWith("SELECT") | query.StartsWith("select"))
+                            cmd.CommandType = CommandType.Text;
+                        else
+                            cmd.CommandType = CommandType.StoredProcedure;
+
+
+
+                        int i;
+                        for (i = 0; i < parameters.Length; i++)
+                            cmd.Parameters.Add(parameters[i]);
+
+
+
+
+                        try
+                        {
+                            cnn.Open();
+                            retval = cmd.ExecuteScalar();
+
+
+
+                        }
+                        catch (Exception ex)
+                        {
+                            retval = null;
+
+
+
+                        }
+                        finally
+                        {
+                            if (cnn.State == ConnectionState.Open) cnn.Close();
+                        }
+                    }
+                }
+                return retval;
+            }
+
+            public SqlDataReader ExecuteReaderSP(String query, SqlParameter[] parameters)
+            {
+                SqlDataReader dr = null;
+                try
+                {
+                    SqlConnection cnn = new SqlConnection(Getcon());
+                    SqlCommand cmd = new SqlCommand(query, cnn);
+                    if (query.StartsWith("SELECT") | query.StartsWith("select"))
+                        cmd.CommandType = CommandType.Text;
+                    else
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                    int i;
+                    for (i = 0; i < parameters.Length; i++)
+                        cmd.Parameters.Add(parameters[i]);
+
+                    cnn.Open();
+                    cmd.CommandTimeout = 180;
+                    dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                }
+                catch (Exception ex)
+                {
+                    dr = null;
+
+                }
+                return dr;
+            }
+
+         
+          
+
+
+        }
+    }
+}
